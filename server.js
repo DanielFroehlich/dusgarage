5660341ff737c36d760c8bc623985a42f9a95456
Daniel Fröhlich <daniel.froehlich@redhat.com>
2017-01-25 19:35:45 +0100

    Broadcast of changes via websockets implemented.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const debug = require('debug')('dusgarage')
-var express = require('express');
-var cron = require('node-cron');
-var app = express();
-var fs = require("fs");
+var express  = require('express');
+var app   = express();
+var server = require('http').createServer(app);
+var io    = require('socket.io').listen(server);
+var cron  = require('node-cron');
+var fs    = require("fs");
 
 var parkinglots;
 var arrayOfStates;
@@ -25,9 +27,21 @@ function resetStates() {
   console.log('resetting all states to initial state %s', arrayOfStates[0].name);
   for (i=0; i<parkinglots.length; i++ ) {
     parkinglots[i].state = arrayOfStates[0];
-    }
+  }
+  parkinglotModified();
 }
 
+function parkinglotModified() {
+  debug("Persisting state...")
+  fs.writeFile( __dirname + "/" + "parkinglots.json", JSON.stringify(parkinglots, null, 2), function(err) {
+     if (err) {
+        return console.error(err);
+     }
+   })
+
+  debug("Broadcast state change to %s connected clients...", io.engine.clientsCount)
+  io.sockets.emit('update', parkinglots);
+}
 
 app.get('/parkinglots', function (req, res) {
     debug("get parkinglots")
@@ -38,45 +52,53 @@ app.get('/parkinglots/:id', function (req, res) {
   res.end( JSON.stringify(parkinglots[req.params.id]));
 })
 
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/client.html');
+});
+
 app.post('/parkinglots/:id', function (req, res) {
-  debug("post parkinglots id=%s", req.params.id)
+  debug("post parkinglots id=%s", req.params.id);
+  parkinglot = parkinglots[req.params.id];
+  parkinglot.state = getNextState(parkinglot.state);
 
-  parkinglot = parkinglots[req.params.id]
-  parkinglot.state = getNextState(parkinglot.state)
+  parkinglotModified();
 
-  // Write back state file:
-  fs.writeFile( __dirname + "/" + "parkinglots.json", JSON.stringify(parkinglots, null, 2), function(err) {
-     if (err) {
-        return console.error(err);
-     }
-   })
-
-  res.end( JSON.stringify(parkinglots[req.params.id]))
+  res.end( JSON.stringify(parkinglot));
 })
 
 
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
+io.sockets.on('connection', function (socket) {
+  debug("websocket client connected");
+  // Send update to this client in case client was disconnected for a long time
+  // and state has changed meanwhile:
+  socket.emit('update', parkinglots);
 
-  console.log("Reading parkinglots")
-  fs.readFile( __dirname + "/" + "parkinglots.json", 'utf8', function (err, data) {
-      parkinglots = JSON.parse(data)
-    });
+  socket.on('disconnect', function () {
+    debug("websocket client disconnect");
+  });
+});
 
-  console.log("Reading states")
-  fs.readFile( __dirname + "/" + "states.json", 'utf8', function (err, data) {
-      arrayOfStates = JSON.parse(data)
+
+// ------------- INIT ----------------
+
+console.log("Reading state...")
+fs.readFile( __dirname + "/" + "parkinglots.json", 'utf8', function (err, data) {
+    parkinglots = JSON.parse(data)
   });
 
-  console.log("Serving static content")
-  app.use('/', express.static(__dirname + '/public'));
+console.log("Reading states...")
+fs.readFile( __dirname + "/" + "states.json", 'utf8', function (err, data) {
+    arrayOfStates = JSON.parse(data)
+});
 
-  console.log("Example app listening at http://%s:%s", host, port)
+console.log("Creating server socket...")
+server.listen(8081);
 
-})
 
+console.log("Scheduling cronjob....")
  // ss mm hh day month dayOfWeek
 cron.schedule('* 42 04 * * *', function(){
   resetStates()
 });
+
+console.log("Server startup completed! Listening at http://%s:%s", server.address().address, server.address().port)
